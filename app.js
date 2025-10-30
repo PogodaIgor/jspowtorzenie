15bdefd06937b163edf4368a376cc6332b6ec632
PogodaIgor <igor.pogoda2010@wp.pl>
2025-10-30 08:45:48 +0100

    a

---
+++ app.js
@@ -0,0 +1,23 @@
+function czerwony(){
+    document.getElementById("czerwony").innerHTML;
+    document.getElementById("body").style.backgroundColor = "red"
+}
+function niebieski(){
+    document.getElementById("niebieski").innerHTML;
+    document.getElementById("body").style.backgroundColor = "blue"
+}
+function zielony(){
+    document.getElementById("zielony").innerHTML;
+    document.getElementById("body").style.backgroundColor = "green"
+}
+function pokaz(){
+    document.getElementById("tekst").innerHTML.display.none;
+    document.getElementById("tekst").style.display = none;
+    document.getElementById("tekst").style.display = block;
+}
+let licznik = 0
+
+function licznik(){
+    licznik = licznik + 1;
+    document.getElementById("licznik").innerHTML;
+}
